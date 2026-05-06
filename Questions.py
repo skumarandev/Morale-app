2b073a875e04b6a589d5e880945a73a1c45b41c9
skumarandev <kumaran.subramaniam@gmail.com>
2026-05-06 08:10:25 +0000

    Version2.3

--- Questions.py
+++ Questions.py
@@ -59,11 +59,8 @@ def main():
             save_text_response(motivation.strip(), expectations.strip())
             st.success('Thank you! Your answers have been saved.')
 
-    #st.write('---')
-    #st.info('After submitting your responses, open the SavedData page to review the text answers in the separate table.')
 if st.button("Go to Admin Page"):
     st.switch_page("pages/SavedData.py")    
-
 
 if __name__ == '__main__':
     main()
